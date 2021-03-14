21cc0a5be82efbce7f01cdcf33a7dd8d654f1fc3
Ibrahim-Arif <ibrahimrana232@gmail.com>
2021-03-14 14:19:47 +0500

    loading screen improved

--- app/screens/LoadingScreen.js
+++ app/screens/LoadingScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, ActivityIndicator, StyleSheet } from "react-native";
 
-function LoadingScreen({ color }) {
+function LoadingScreen({ color = "dodgerblue" }) {
   return (
     <View style={styles.container}>
       <ActivityIndicator color={color} size={50} />
