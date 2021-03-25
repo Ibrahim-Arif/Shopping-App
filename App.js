4cae3b7baadc74945e0bb7fa1979d7e55e326a65
Ibrahim-Arif <ibrahimrana232@gmail.com>
2021-03-25 23:03:16 +0500

    login using secure storage user data

--- App.js
+++ App.js
@@ -13,24 +13,32 @@ export default function App() {
   const [isReady, setIsReady] = useState(false);
   const [user, setUser] = useState();
 
+  const initFirebase = () => {
+    firebase.initializeApp({
+      apiKey: "AIzaSyC2EAH4qe28zaaZQ6AWq8jW0mdSafYyids",
+      authDomain: "dawn-bcee8.firebaseapp.com",
+      databaseURL: "https://dawn-bcee8-default-rtdb.firebaseio.com",
+      projectId: "dawn-bcee8",
+      storageBucket: "dawn-bcee8.appspot.com",
+      messagingSenderId: "816524272637",
+      appId: "1:816524272637:web:48c96805b9ebb8484baf94",
+    });
+  };
+
   useEffect(() => {
-    if (!firebase.app.length) {
-      firebase.initializeApp({
-        apiKey: "AIzaSyC2EAH4qe28zaaZQ6AWq8jW0mdSafYyids",
-        authDomain: "dawn-bcee8.firebaseapp.com",
-        databaseURL: "https://dawn-bcee8-default-rtdb.firebaseio.com",
-        projectId: "dawn-bcee8",
-        storageBucket: "dawn-bcee8.appspot.com",
-        messagingSenderId: "816524272637",
-        appId: "1:816524272637:web:48c96805b9ebb8484baf94",
-      });
-    }
+    if (!firebase.app.length) initFirebase();
   }, []);
 
   const authUser = async () => {
     const result = await secureStorage.readUser();
     if (!result) return;
-    setUser(JSON.parse(result));
+
+    const user = JSON.parse(result);
+    initFirebase();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(user.email, user.password)
+      .then(() => setUser(user));
   };
 
   if (!isReady) {
